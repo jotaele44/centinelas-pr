6dad4fc9455fdae77d9f83bb741d066f6558e60f
jotaele44 <jorge.luis1352@gmail.com>
2026-07-27 03:05:15 -0400

    Add releases subsystem

---
+++ src/centinelas/releases/__init__.py
@@ -0,0 +1,39 @@
+"""Post-freeze federal document release monitoring.
+
+Centinelas is the sole acquisition authority for public federal document releases.
+The July 27, 2026 census baseline is immutable; later observations are versioned deltas.
+"""
+
+from .models import (
+    BASELINE_CUTOFF,
+    AcquisitionReceipt,
+    BaselineManifest,
+    DeltaManifest,
+    DocumentFinding,
+    FederalDocument,
+    FederalDocumentRelease,
+    ReleaseAdapter,
+    ReleaseState,
+    SyntheticReleaseAdapter,
+    classify_release,
+    deterministic_id,
+    sha256_bytes,
+    write_immutable_manifest,
+)
+
+__all__ = [
+    "BASELINE_CUTOFF",
+    "AcquisitionReceipt",
+    "BaselineManifest",
+    "DeltaManifest",
+    "DocumentFinding",
+    "FederalDocument",
+    "FederalDocumentRelease",
+    "ReleaseAdapter",
+    "ReleaseState",
+    "SyntheticReleaseAdapter",
+    "classify_release",
+    "deterministic_id",
+    "sha256_bytes",
+    "write_immutable_manifest",
+]
